96949e3cf29651ad59a643717cb3619503a78a62
Senan Riley <126216229+LilPopcat@users.noreply.github.com>
2023-03-12 18:59:05 -0700

    bison

--- snake.js
+++ snake.js
@@ -41,7 +41,7 @@ function update () {
     context.fillStyle="black";
     context.fillRect(0, 0, board.width, board.height);
 
-    context.fillStyle="red";
+    context.fillStyle="brown";
     context.fillRect(foodX, foodY, blockSize, blockSize);
 
     if (snakeX == foodX && snakeY == foodY) {
