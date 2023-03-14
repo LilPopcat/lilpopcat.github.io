38c658ecfe4fa02e51dc36440619faa2875440e5
Senan Riley <126216229+LilPopcat@users.noreply.github.com>
2023-03-13 17:09:08 -0700

    alert added back in

--- snake.js
+++ snake.js
@@ -57,6 +57,7 @@ function update () {
     //game over conditions
     if (snakeX < 0 || snakeX > cols*blockSize || snakeY < 0 || snakeY > rows*blockSize) {
         gameOver = false;
+        alert("Game Over");
     }
 }
 
